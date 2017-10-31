4110a51236192f5d2e5758694a44ab255e4624d0
AllanLa <al037@bucknell.edu>
2017-10-31 15:16:44 -0400

    base code set up almost

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//var divRoot = $("#affdex_elements")[0];
+var divRoot = document.getElementById("affdex_elements");
+var width = 640;
+var height = 480;
+var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
+detector.detectAllEmotions();
+detector.detectAllExpressions();
+detector.detectAllEmojis();
+detector.detectAllAppearance();
+
+function onStart() {
+	if (detector && !detector.isRunning) {
+		console.log("Detector Started");
+		detector.start();
+     }     
+ }
+
+function generateMeme() {
+	//have javascript to log in now
+	console.log("Generating Meme");
+}
+
+function generateQuotes() {
+	//have javascript to log in now
+	console.log("Generating Quotes");
+}
+
