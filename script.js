6c6838fda57f952f38f06a07e9f9cd20dae6a83e
AllanLa <al037@bucknell.edu>
2017-11-02 15:53:58 -0400

    Random Meme Generates

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // SDK Needs to create video and canvas nodes in the DOM in order to function
 // Here we are adding those nodes a predefined div.
+var generated = false;
+
 var divRoot = $("#affdex_elements")[0];
 var width = 680;
 var height = 480;
@@ -121,9 +123,116 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image,
 }*/
 
 function generateGif() {
-    //have javascript to log in now
+    if (generated){    
+        removePrevious();
+    }
+
     giphyData = {}
     maxVal = -1
+    maxEmotion = findMaxEmotion();
+    console.log("Max Emotion: " + maxEmotion);
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + maxEmotion + "&api_key=0MVjiemZMnv3N9Oh55IpB8SeYSf1ugLd&limit=1");
+    xhr.done(function(data) {
+        giphyEmbedUrl = data.data[0].embed_url;
+    });
+    $("#gifDiv").html("<div style='width:50%;height:0;padding-bottom:20%;position:relative;'><iframe src='" + giphyEmbedUrl + "' width='100%' height='100%' style='position:absolute' frameBorder='0' class='giphy-embed' allowFullScreen></iframe></div>")
+
+    generated = true;
+}
+
+
+//joy , sadness, disgust , anger, surprise
+
+function generateMeme() {
+
+    maxEmotion = getRandomEmotion(); //findMaxEmotion();
+
+    if (generated){    
+        removePrevious();
+    }
+
+    var img = new Image();
+    var div = document.getElementById('gifDiv');
+
+    img.onload = function() {
+      div.appendChild(img);
+    };    
+    
+    var random;
+    if (maxEmotion == "joy"){
+        random = getRandomHappyImage();
+        img.src = 'Memes/Happy/'+random;
+    } 
+
+    else if (maxEmotion == "sadness"){
+        random = getRandomSadImage();
+        img.src = 'Memes/Sad/'+random;        
+    }
+
+    else if (maxEmotion == "anger"){
+        random = getRandomAngerImage();
+        img.src = 'Memes/Anger/'+random;        
+    }
+
+    else if (maxEmotion == "disgust"){
+        random = getRandomDigustImage();
+        img.src = 'Memes/Disgust/'+random;        
+    }
+
+    else{
+        random = getRandomSurpriseImage();
+        img.src = 'Memes/Surprise/'+random;        
+    }
+
+    generated = true;
+}
+
+
+function generateQuotes() {
+    //have javascript to log in now
+    if (generated){    
+        removePrevious();
+    }    
+    console.log("Generating Quotes");
+    generated = true;
+}
+
+function getRandomEmotion(){
+  var images = ["joy", "sadness", "surprise", "disgust", "anger" ];
+  return images[Math.floor(Math.random() * images.length)];    
+}
+
+function getRandomHappyImage() {
+  var images = ["happy1.png","happy2.png","happy3.jpg", "happy4.jpg", "happy5.jpg" ];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function getRandomSadImage() {
+  var images = ["sad1.jpg","sad2.jpg","sad3.jpg", "sad4.jpg", "sad5.jpg", "sad6.jpg", "sad7.jpg", "sad8.jpg" ];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function getRandomAngerImage() {
+  var images = ["AngerMeme1.jpg","AngerMeme2.jpg","AngerMeme3.jpg", "AngerMeme4.jpeg", "AngerMeme5.jpg", "AngerMeme6.jpg", "AngerMeme7.jpg", "AngerMeme8.jpg" ];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function getRandomDigustImage() {
+  var images = ["DisgustMeme1.jpg","DisgustMeme2.jpeg","DisgustMeme3.jpg", "DisgustMeme4.jpeg", "DisgustMeme5.jpg", "DisgustMeme6.jpg"];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function getRandomSurpriseImage() {
+  var images = ["SurpriseMeme1.jpg","SurpriseMeme2.gif","SurpriseMeme3.jpg", "SurpriseMeme4.jpeg"];
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function removePrevious(){
+    var remove = document.getElementById('gifDiv');
+    remove.removeChild(remove.childNodes[0]);
+}
+
+function findMaxEmotion(){
     emotionData["engagement"] = 0;
     emotionData["valence"] = 0;
     for (var key in emotionData) {
@@ -140,16 +249,5 @@ function generateGif() {
             }
         }
     }
-    console.log("Max Emotion: " + maxEmotion);
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + maxEmotion + "&api_key=0MVjiemZMnv3N9Oh55IpB8SeYSf1ugLd&limit=1");
-    xhr.done(function(data) {
-        giphyEmbedUrl = data.data[0].embed_url;
-    });
-    $("#gifDiv").html("<div style='width:50%;height:0;padding-bottom:20%;position:relative;'><iframe src='" + giphyEmbedUrl + "' width='100%' height='100%' style='position:absolute' frameBorder='0' class='giphy-embed' allowFullScreen></iframe></div>")
-
-}
-
-function generateQuotes() {
-    //have javascript to log in now
-    console.log("Generating Quotes");
+    return maxEmotion;
 }
